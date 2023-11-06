32d524efc805efe7181c5e283d5cde6c835c84da
Mehdi MADRANI <mehdimadrani@Mehdis-MacBook-Pro.local>
2023-11-06 13:29:25 +0100

    fixing port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ejs from "ejs";
 
 
 const app = express();
-const port = 3000 ;
+const port = process.env.PORT || 3000; 
 const APIKey = "2a637689b9f5547704a4310298d0d666";
 
 app.set("view engine", "ejs");
